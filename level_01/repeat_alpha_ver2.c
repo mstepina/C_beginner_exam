c0289904a280d2eebfc2869c3edf73405c33a2dd
mstepina <50503381+mstepina@users.noreply.github.com>
2019-07-01 17:19:07 -0700

    Update repeat_alpha_ver2.c

--- level_01/repeat_alpha_ver2.c
+++ level_01/repeat_alpha_ver2.c
@@ -22,12 +22,11 @@ int main(int ac, char **av)
 	{
 		while(av[1][i])
 		{
+			count = 1;
 			if (av[1][i] >= 'a' && av[1][i] <= 'z')
 				count = av[1][i] - 'a' + 1;
 			else if (av[1][i] >= 'A' && av[1][i] <= 'Z')
-				count = av[1][i] - 'A' + 1;
-			else
-				count = 1;
+				count = av[1][i] - 'A' + 1;	
 			while (count > 0)	
 			{
 				write(1, &av[1][i], 1);
