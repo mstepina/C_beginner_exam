1d85042d6a286d70aefc0d91653739e2e8fa0ddd
mstepina <50503381+mstepina@users.noreply.github.com>
2019-07-29 18:26:32 -0700

    add main

--- level_02/swap_bits.c
+++ level_02/swap_bits.c
@@ -15,3 +15,39 @@ unsigned char	swap_bits(unsigned char octet)
 	return (((octet <<  4) | (octet >> 4)));
 }
 
+/*
+#include <stdio.h>
+#include <unistd.h>
+
+int  print_bits(unsigned char octet)
+{
+	int n;
+	int div;
+
+	div = 128;
+	n = octet;
+	while(div != 0)
+	{
+		if (n >= div)
+		{
+			write(1, "1", 1);
+			n = n % div;
+		}
+		else
+			write(1, "0", 1);
+		div = div / 2;
+	}
+	return (0);
+}
+
+int main ()
+{
+	unsigned char n;
+	unsigned char m;
+	n = '9';
+	printf("%c\n", print_bits(n));
+	m = swap_bits(n);
+	printf("%c\n", print_bits(m));
+	return (0);
+}
+*/
