c155ecd510c0e8e405e99cdf34e90726af9055ed
mstepina <50503381+mstepina@users.noreply.github.com>
2019-08-11 21:50:12 -0700

    Update ft_putstr.c

--- level_01/ft_putstr.c
+++ level_01/ft_putstr.c
@@ -18,10 +18,14 @@ void	ft_putstr(char *str)
 
 	i = 0;
 	while(str[i])
-	{	
-		write(1, &str[i], 1);
-		i++;
-	}
+		write(1, &str[i++], 1);
 }
 
-
+/*
+int main(int ac, char **av)
+{
+	if (ac == 2)
+		ft_putstr(av[1]);
+	return (0);
+}
+*/
